a5d943b9e3133aee58db39ab0698e6b0ad5e9e72
Ricky Dodd <rickydoddmobile@gmail.com>
2023-12-14 15:39:10 +1100

    refact: remove magic numbers, rename variables
    Removed magic numbers and replaced with constants. Renamed some
    variables so that their name matched their usage.

--- food-lines/src/food_lines.c
+++ food-lines/src/food_lines.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int get_shortest_line_index(int n[100], int n_lines);
+#define MAX_LINES 100       // maximum number of lines
+#define MAX_CHARS 10100     // the maximum number of characters to read during input (null char inclusive)
+
+int get_shortest_line_index(int lines[MAX_LINES], int n_lines);
 
 int main(void) {
-    int i = 0, numbers[100], n_lines, n_new_people, shortest_line_index;
-    char line[10100], *end;
+    int i = 0, lines[MAX_LINES], n_lines, n_new_people, shortest_line_index;
+    char line[MAX_CHARS], *end;
     long int li;
 
     // get two whitespace separated integers from stdin.
@@ -17,7 +20,7 @@ int main(void) {
     fgets(line, sizeof(line), stdin);
     end = line;
     while((li = strtol(end, &end, 10))) {
-        numbers[i] = (int)li;
+        lines[i] = (int)li;
         i++;
     }
 
@@ -26,17 +29,17 @@ int main(void) {
         if (n_lines == 1) {
             shortest_line_index = 0;
         } else {
-            shortest_line_index = get_shortest_line_index(numbers, n_lines);
+            shortest_line_index = get_shortest_line_index(lines, n_lines);
         }
-        printf("%d\n", numbers[shortest_line_index]);
-        numbers[shortest_line_index]++;
+        printf("%d\n", lines[shortest_line_index]);
+        lines[shortest_line_index]++;
     }
 
     return 0;
 }
 
 // linear search for the shortest line
-int get_shortest_line_index(int n[100], int n_lines) {
+int get_shortest_line_index(int lines[MAX_LINES], int n_lines) {
     int shortest_line_index = 0;
     for (int i = 1; i < n_lines; i++) {
         if (n[shortest_line_index] > n[i]) {
